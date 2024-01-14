a5f0d4c27ef49c96368860d3913497f67c74c50d
DaniellBlach <danys9987@gmail.com>
2024-01-14 18:21:54 +0100

    Solve day1 part2

--- src/day_01.py
+++ src/day_01.py
@@ -18,9 +18,34 @@ async def return_part_one_output():
     ]
 
 
+@router.get("/day1/part2")
+async def return_part_one_output():
+    return [
+        {"result": solve_part_two()}
+    ]
+
+
 def solve_part_one() -> int:
     result = 0
     for line in calibrations:
         string = ''.join(char for char in line if char.isdigit())
         result += int(string[0] + '' + string[-1])
     return result
+
+
+def solve_part_two() -> int:
+    numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+    result = 0
+
+    for line in calibrations:
+        nums = []
+        for char in range(len(line)):
+            if (line[char].isdigit()):
+                nums.append(line[char])
+            else:
+                for number in numbers:
+                    if line[char:char + len(number)] == number:
+                        nums.append(numbers[number])
+        result += int(str(nums[0]) + '' + str(nums[-1]))
+    return result
+
