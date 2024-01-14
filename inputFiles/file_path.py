2ad25329b68ab59dbe835b63c42c2cd914fab30a
DaniellBlach <danys9987@gmail.com>
2024-01-14 13:03:33 +0100

    added file_path file which will return global path for project

---
+++ inputFiles/file_path.py
@@ -0,0 +1,6 @@
+import os
+
+# Look to the path of your current working directory
+working_directory = os.getcwd()
+# Or: file_path = os.path.join(working_directory, 'my_file.py')
+file_path = working_directory + '\\inputFiles\\'
