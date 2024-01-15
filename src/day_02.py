c67ece47ed5440ac470ebd642c9f32132621fb05
DaniellBlach <dandys9987@gmail.com>
2024-01-15 17:48:08 +0100

    solve day2 part2

--- src/day_02.py
+++ src/day_02.py
@@ -18,6 +18,13 @@ async def return_part_one_output():
     ]
 
 
+@router.get("/day2/part2")
+async def return_part_two_output():
+    return [
+        {"result": solve_part_two()}
+    ]
+
+
 def solve_part_one() -> int:
     rules = {"red": 12, "green": 13, "blue": 14}
     result = 0
@@ -31,4 +38,24 @@ def solve_part_one() -> int:
                 valid_value = False
         if valid_value:
             result += count + 1
+    return result
+
+
+def solve_part_two() -> int:
+    result = 0
+    for count, game in enumerate(game_records):
+        results = game.split(":")
+        sets = results[1].replace('; ', ', ').split(",")
+        red = []
+        green = []
+        blue = []
+        for cubes in sets:
+            cubes = cubes[1:].split(" ")
+            if cubes[1] == "red":
+                red.append(int(cubes[0]))
+            elif cubes[1] == "green":
+                green.append(int(cubes[0]))
+            elif cubes[1] == "blue":
+                blue.append(int(cubes[0]))
+        result += (max(red) * max(green) * max(blue))
     return result
