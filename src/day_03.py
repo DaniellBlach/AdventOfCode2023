e33b90179e8fd19ad2ee46c621f73ebec801a848
DaniellBlach <dandys9987@gmail.com>
2024-01-18 19:15:52 +0100

    Modify find_adjacent_symbol method so i could use it in second part

--- src/day_03.py
+++ src/day_03.py
@@ -43,7 +43,7 @@ def solve_part_one() -> int:
                 digit += engine_schematic[i][j]
             elif len(digit) > 0 and not engine_schematic[i][j].isdigit():
                 adjacent_symbol = find_adjacent_symbol(i, j - len(digit), j - 1)
-                if adjacent_symbol:
+                if adjacent_symbol != 0:
                     result += int(digit)
                 digit = ""
     return result
@@ -52,18 +52,18 @@ def solve_part_one() -> int:
 def find_adjacent_symbol(row: int, begin: int, end: int):
     # RIGHT
     if end + 1 < chars_count and engine_schematic[row][end + 1] != "." and not engine_schematic[row][end + 1].isdigit():
-        return True
+        return str(row) + "." + str(end)
     # LEFT
     if begin - 1 > 0 and engine_schematic[row][begin - 1] != "." and not engine_schematic[row][begin - 1].isdigit():
-        return True
+        return str(row) + "." + str(end)
     # TOP
     if row - 1 > 0:
         for j in range(max(0, begin - 1), min(end + 1, chars_count) + 1):
             if engine_schematic[row - 1][j] != "." and not engine_schematic[row - 1][j].isdigit():
-                return True
+                return str(row) + "." + str(end)
     # BOTTOM
     if row + 1 < rows_count:
         for j in range(max(0, begin - 1), min(end + 1, rows_count) + 1):
             if engine_schematic[row + 1][j] != "." and not engine_schematic[row + 1][j].isdigit():
-                return True
-    return False
+                return str(row) + "." + str(end)
+    return 0
