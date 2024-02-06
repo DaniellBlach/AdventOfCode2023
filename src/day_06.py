8188204de5daa33743565582ccfced903747574c
DaniellBlach <dandys9987@gmail.com>
2024-02-06 17:45:03 +0100

    Solve day 6 part 2

--- src/day_06.py
+++ src/day_06.py
@@ -10,8 +10,6 @@ router = APIRouter(
 )
 file = open(fp.file_path + 'day_06.txt', "r")
 data = file.read().splitlines()
-times = list(map(int, data[0].split()[1:]))
-distances = list(map(int, data[1].split()[1:]))
 
 
 @router.get("/day6/part1")
@@ -21,12 +19,31 @@ async def return_part_one_output():
     ]
 
 
+@router.get("/day6/part2")
+async def return_part_two_output():
+    return [
+        {"result": solve_part_two()}
+    ]
+
+
 def solve_part_one() -> int:
     result = 1
+    times = list(map(int, data[0].split()[1:]))
+    distances = list(map(int, data[1].split()[1:]))
     for index, time in enumerate(times):
         possibilities = 0
         for second in range(1, time):
             if second * (time - second) > distances[index]:
                 possibilities += 1
         result *= possibilities
+    return result
+
+
+def solve_part_two() -> int:
+    result = 0
+    times = int("".join(data[0].split()[1:]))
+    distances = int("".join(data[1].split()[1:]))
+    for second in range(1, times):
+        if second * (times - second) > distances:
+            result += 1
     return result
